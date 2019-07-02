d7c10336e4f9230f7c11fec231a90e6638b29104
Avery Crespi <averycrespi@gmail.com>
2019-07-02 13:28:27 -0700

    Remove prune from yovec

--- yovec.py
+++ yovec.py
@@ -1,7 +1,6 @@
 from lark import Lark
 
 from engine.node import Node
-from engine.yovec.prune import prune
 
 
 with open('grammar/yovec.ebnf') as f:
@@ -10,8 +9,6 @@ parser = Lark(grammar, start='program')
 
 with open('sample/dist.yovec') as f:
     raw_program = f.read()
-program = parser.parse(raw_program)
-program = Node.from_tree(program)
-program = prune(program)
+program = Node.from_tree(parser.parse(raw_program))
 
 print(program.pretty())
