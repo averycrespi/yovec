70495d7f8872c83412a083e9ca8558cdd6a217f6
Avery Crespi <averycrespi@gmail.com>
2019-07-01 21:09:58 -0700

    Start yovec transpile

---
+++ engine/yovec/transpile.py
@@ -0,0 +1,9 @@
+from engine.env import Env
+from engine.node import Node
+
+
+def transpile(program):
+    """Transpile a Yovec program to YOLOL."""
+    env = Env(overwrite=False)
+    #TODO: implement
+    return None
