c7ae485bbc0dcb4cf1499dfe611b310c644fd20f
Avery Crespi <averycrespi@gmail.com>
2019-07-01 17:21:45 -0700

    Add yovec graph

--- yovec.py
+++ yovec.py
@@ -1,5 +1,8 @@
 from lark import Lark
 
+from engine.node import Node
+from engine.yovec.dep import graph
+
 
 with open('grammar/yovec.ebnf') as f:
     grammar = f.read()
@@ -7,6 +10,7 @@ parser = Lark(grammar, start='program')
 
 with open('example/dist.yovec') as f:
     raw_program = f.read()
-program = parser.parse(raw_program)
+program = Node.from_tree(parser.parse(raw_program))
 
 print(program.pretty())
+print(graph(program))
