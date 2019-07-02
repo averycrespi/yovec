834b43c0a272974b5bb601588a59f48088899f35
Avery Crespi <averycrespi@gmail.com>
2019-07-02 09:59:25 -0700

    Update yovec transpile

--- engine/yovec/transpile.py
+++ engine/yovec/transpile.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 from engine.env import Env
 from engine.node import Node
 
@@ -5,5 +7,13 @@ from engine.node import Node
 def transpile(program: Node) -> Node:
     """Transpile a Yovec program to YOLOL."""
     env = Env(overwrite=False)
-    #TODO: implement
-    return None
+    children = []
+    for line in program.children:
+        env, out_line = _transpile_line(env, line)
+        children.append(out_line)
+    return Node(kind='program', children=children)
+
+
+def _transpile_line(env: Env, line: Node) -> Tuple[Env, Node]:
+    """Transpile a line."""
+    pass #TODO: implement
