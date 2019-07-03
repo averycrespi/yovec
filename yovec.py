dd8834ced4f60ca7bd484457350e886394d81842
Avery Crespi <averycrespi@gmail.com>
2019-07-03 11:10:04 -0700

    Update yovec program

--- yovec.py
+++ yovec.py
@@ -1,3 +1,5 @@
+from argparse import ArgumentParser
+
 from lark import Lark
 
 from engine.node import Node
@@ -5,13 +7,23 @@ from engine.yolol.format import format
 from engine.yovec.transpile import transpile
 
 
-with open('grammar/yovec.ebnf') as f:
-    grammar = f.read()
-parser = Lark(grammar, start='program')
+def parse_args():
+    parser = ArgumentParser(description='Transpile Yovec to YOLOL')
+    parser.add_argument('srcfile', type=str, help='Yovec source file')
+    return parser.parse_args()
 
-with open('sample/dist.yovec') as f:
-    raw_program = f.read()
-yovec_program = Node.from_tree(parser.parse(raw_program))
-yolol_program = transpile(yovec_program)
 
-print(format(yolol_program))
+def main(srcfile):
+    with open('grammar/yovec.ebnf') as f:
+        grammar = f.read()
+    parser = Lark(grammar, start='program')
+    with open(srcfile) as f:
+        raw_program = f.read()
+    yovec_program = Node.from_tree(parser.parse(raw_program))
+    yolol_program = transpile(yovec_program)
+    print(format(yolol_program))
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args.srcfile)
