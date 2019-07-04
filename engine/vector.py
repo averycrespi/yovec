fbf2030a537cdd6da3b34e1873fc27a2c13479d5
Avery Crespi <averycrespi@gmail.com>
2019-07-04 09:10:40 -0700

    Remove queue from SimpleVector

--- engine/vector.py
+++ engine/vector.py
@@ -10,55 +10,43 @@ from engine.number import SimpleNumber
 class SimpleVector:
     """Represents a list of simple numbers."""
     def __init__(self, snums: List[SimpleNumber]):
-        self.initial = snums
-        self.length = len(self.initial)
-        self.queue = []
+        self.snums = snums
+        self.length = len(snums)
 
     # Operations
 
     def vecunary(self, op: str) -> 'SimpleVector':
         """Apply a unary operation to a simple vector."""
-        result = deepcopy(self)
-        result.queue.append((op,))
-        return result
+        return SimpleVector([sn.unary(op.strip('vec_')) for sn in self.snums])
 
-    def vecbinary(self, op: str, sv: 'SimpleVector') -> 'SimpleVector':
+    def vecbinary(self, op: str, other: 'SimpleVector') -> 'SimpleVector':
         """Apply a binary operation to two simple vectors."""
-        if self.length != sv.length:
+        if self.length != other.length:
             raise YovecError('cannot apply operation "{}" to vectors of different lengths'.format(op))
-        result = deepcopy(self)
-        result.queue.append((op, sv))
-        return result
+        return SimpleVector([sn.binary(op.strip('vec_'), other.snums[i]) for i, sn in enumerate(self.snums)])
 
     def map(self, op: str) -> 'SimpleVector':
         """Map a unary operation to a simple vector."""
-        result = deepcopy(self)
-        result.queue.append(('vec_map', op))
-        return result
+        return SimpleVector([sn.unary(op) for sn in self.snums])
 
-    def premap(self, op: str, sn: SimpleNumber) -> 'SimpleVector':
+    def premap(self, op: str, other: SimpleNumber) -> 'SimpleVector':
         """Premap a binary operation to a simple vector."""
-        result = deepcopy(self)
-        result.queue.append(('vec_premap', op, sn))
-        return result
+        return SimpleVector([sn.binary(op, other) for sn in self.snums])
 
-    def postmap(self, sn: SimpleNumber, op: str) -> 'SimpleVector':
+    def postmap(self, other: SimpleNumber, op: str) -> 'SimpleVector':
         """Postmap a binary operation to a simple vector."""
-        result = deepcopy(self)
-        result.queue.append(('vec_postmap', sn, op))
-        return result
+        return SimpleVector([other.binary(op, sn) for sn in self.snums])
 
-    def concat(self, sv: 'SimpleVector') -> 'SimpleVector':
+    def concat(self, other: 'SimpleVector') -> 'SimpleVector':
         """Concatenate two simple vectors."""
-        initial = [*self.resolve(), *sv.resolve()]
-        return SimpleVector(initial)
+        return SimpleVector([*self.snums, *other.snums])
 
-    def dot(self, sv: 'SimpleVector') -> SimpleNumber:
+    def dot(self, other: 'SimpleVector') -> SimpleNumber:
         """Calculate the dot product of two simple vectors."""
-        result = SimpleNumber(0)
-        for lsn, rsn in zip(self.resolve(), sv.resolve()):
-            result = result.binary('add', lsn.binary('mul', rsn))
-        return result
+        sn = SimpleNumber(0)
+        for lsn, rsn in zip(self.snums, other.nums):
+            sn = result.binary('add', lsn.binary('mul', rsn))
+        return sn
 
     def len(self) -> SimpleNumber:
         """Return the length of the simple vector."""
@@ -66,41 +54,20 @@ class SimpleVector:
 
     def reduce(self, op: str) -> SimpleNumber:
         """Reduce the simple vector to a simple number."""
-        snums = self.resolve()
-        result = snums[0]
-        for sn in snums[1:]:
-            result = result.binary(op, sn)
-        return result
+        lsn = self.snums[0]
+        for rsn in self.snums[1:]:
+            lsn = lsn.binary(op, rsn)
+        return lsn
 
     # Resolutions
 
-    def resolve(self) -> List[SimpleNumber]:
-        """Resolve operations for each simple number."""
-        results = []
-        for i, sn in enumerate(self.initial):
-            for op, *args in self.queue:
-                if op == 'vec_map':
-                    sn = sn.unary(args[0])
-                elif op == 'vec_premap':
-                    sn = sn.binary(args[0], args[1])
-                elif op == 'vec_postmap':
-                    sn = args[0].binary(args[1], sn)
-                elif len(args) == 0: # vec_unary
-                    sn = sn.unary(op.strip('vec_'))
-                elif len(args) == 1: # vec_binary
-                    sn = sn.binary(op.strip('vec_'), args[0].resolve()[i])
-                else:
-                    raise ValueError('unrecognized item in queue: {}, {}'.format(op, args))
-            results.append(sn)
-        return results
-
-    def assign(self, index: int) -> Tuple[List[Node], 'SimpleVector']:
+    def assign(self, vec_index: int) -> Tuple[List[Node], 'SimpleVector']:
         """Generate YOLOL assignment statements."""
         assignments = []
         snums = []
-        expressions = [sn.evaluate() for sn in self.resolve()]
+        expressions = [sn.evaluate() for sn in self.snums]
         for i, expr in enumerate(expressions):
-            ident = 'v{}e{}'.format(index, i)
+            ident = 'v{}e{}'.format(vec_index, i)
             var = Node(kind='variable', value=ident)
             asn = Node(kind='assignment', children=[var, expr])
             assignments.append(asn)
