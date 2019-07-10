8a987898d66032de7d51f1e34849c11e56774729
Avery Crespi <averycrespi@gmail.com>
2019-07-09 21:44:54 -0700

    Update version to v1.3.0

--- yovec.py
+++ yovec.py
@@ -10,7 +10,7 @@ from engine.optimize.dead import eliminate_dead_code
 from engine.transpile.yolol import yovec_to_yolol, Context
 
 
-__version__ = 'v1.2.1'
+__version__ = 'v1.3.0'
 
 
 parser = ArgumentParser(description='Transpile Yovec to YOLOL')
