206af07a6756f4772989e99df8000a8a83668c44
Avery Crespi <averycrespi@gmail.com>
2019-07-28 09:02:11 -0700

    Accept input from stdin if -i is unset
    Closes #115

--- yovec-cli.py
+++ yovec-cli.py
@@ -1,7 +1,7 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, FileType
 from os.path import realpath, dirname
 from pathlib import Path
-from sys import stderr, exit
+from sys import stdin, stdout, stderr, exit
 
 from engine.errors import YovecError
 from engine.run import run_yovec
@@ -9,27 +9,27 @@ from engine.version import VERSION
 
 
 parser = ArgumentParser(description='Transpile Yovec to YOLOL')
-parser.add_argument('-i', action='store', dest='infile', default=None, help='Yovec source file')
-parser.add_argument('-o', action='store', dest='outfile', default=None, help='YOLOL output file (stdout if unset)')
+
+parser.add_argument('-i', action='store', dest='infile', default=stdin,
+        type=FileType('r'), help='Yovec source file (stdin if unset)')
+parser.add_argument('-o', action='store', dest='outfile', default=stdout,
+        type=FileType('w'), help='YOLOL output file (stdout if unset)')
+
 parser.add_argument('--ast', action='store_true', help='output Yovec AST (overrides --cylon)')
 parser.add_argument('--cylon', action='store_true', help='output Cylon JSON')
 parser.add_argument('--no-elim', action='store_true', help='disable dead code elimination')
 parser.add_argument('--no-mangle', action='store_true', help='disable name mangling')
 parser.add_argument('--no-reduce', action='store_true', help='disable expression reduction')
 parser.add_argument('--version', action='store_true', help='print version info')
+
 args = parser.parse_args()
 
 if args.version:
     print(VERSION)
     exit(0)
 
-if args.infile is None:
-    parser.print_help()
-    exit(1)
-
 try:
-    with open(args.infile) as f:
-        source = f.read()
+    source = args.infile.read()
 except IOError as e:
     stderr.write('Input error: {}\n'.format(str(e)))
     exit(1)
@@ -51,14 +51,10 @@ except Exception as e:
     stderr.write('Unexpected error: {}\n'.format(str(e)))
     exit(1)
 
-if args.outfile is None:
-    print(output)
-else:
-    try:
-        with open(args.outfile, mode='w') as f:
-            f.write(output)
-    except IOError as e:
-        stderr.write('Output error: {}\n'.format(str(e)))
-        exit(1)
+try:
+    args.outfile.write(output + '\n')
+except IOError as e:
+    stderr.write('Output error: {}\n'.format(str(e)))
+    exit(1)
 
 exit(0)
